22120dc43b1b7f2416e5750adf84dc97729d2224
Ben <1015244@student.sae.edu.au>
2020-06-29 17:06:13 +1000

    UI mockup

---
+++ Iso Movement Prototype/Assets/Scripts/Menu_Options.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_Options : MonoBehaviour
+{
+
+    [SerializeField]
+
+    public GameObject gamePanel;
+    public GameObject auxPanel;
+    public GameObject gfxPanel;
+
+    public GameObject gameText;
+    public GameObject auxText;
+    public GameObject gfxText;
+
+
+    public void ChangeToGameTab()
+    {
+        gfxText.SetActive(false);
+        gfxPanel.SetActive(false);
+        auxText.SetActive(false);
+        auxPanel.SetActive(false);
+        gameText.SetActive(true);
+        gamePanel.SetActive(true);
+
+    }
+
+    public void ChangeToAuxTab()
+    {
+        gfxText.SetActive(false);
+        gfxPanel.SetActive(false);
+        auxText.SetActive(true);
+        auxPanel.SetActive(true);
+        gameText.SetActive(false);
+        gamePanel.SetActive(false);
+    }
+
+    public void ChangeToGFXTab()
+    {
+        gfxText.SetActive(true);
+        gfxPanel.SetActive(true);
+        auxText.SetActive(false);
+        auxPanel.SetActive(false);
+        gameText.SetActive(false);
+        gamePanel.SetActive(false);
+    }
+}
