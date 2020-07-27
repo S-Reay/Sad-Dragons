a2b9ee1eafc0d4e9a54316a25134b253683c0b0e
Seth Reay <1017464@student.sae.edu.au>
2020-07-28 02:24:55 +1000

    Code Block Functions
    -Code can now refence in world objects
    -Code will not run if syntax is not correct
    -Code can now modify in world objects
    -Objects blocks are functional
    -Is blocks are functional
    -Bool blocks are functional
    -Only one StartingBlock can exist in each scene

--- Iso Movement Prototype/Assets/Scripts/Seth/SP_CodeBlock_Info.cs
+++ Iso Movement Prototype/Assets/Scripts/Seth/SP_CodeBlock_Info.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class SP_CodeBlock_Info : MonoBehaviour
 {
-    [Header("TYPE CAN BE: Object, Operator, Int, Bool, If, For, Is")]
+    [Header("TYPE CAN BE: Object, Operator, Int, Bool, If, For, Is, Equals, Float")]
     public string type;
+    public string referencedObjectsTag;
+    public bool _bool;
+
+    private void Awake()
+    {
+        if (type != "Object")               //If this code block is not an object
+        {
+            referencedObjectsTag = null;    //It does not reference any gameObjects via tag
+        }
+    }
 }
--- Iso Movement Prototype/Assets/Scripts/Seth/SP_CodeExecute.cs
+++ Iso Movement Prototype/Assets/Scripts/Seth/SP_CodeExecute.cs
@@ -7,20 +7,44 @@ public class SP_CodeExecute : MonoBehaviour
     [SerializeField] private GameObject startingBlock;
     [SerializeField] private LayerMask codeBlockLayer;
     public List<GameObject> currentBlocks = new List<GameObject>();
+
+    public List<GameObject> objectsToModify1 = new List<GameObject>();
+
     private void Awake()
     {
         startingBlock = GameObject.FindGameObjectWithTag("StartingBlock");
     }
     public void Execute()
     {
-        ListBlocks();       //Detect which blocks are in a row and add them to the list
-        if (SyntaxCheck())  //Determine if syntax is correct
+        objectsToModify1.Clear();    //Clear any previously saved objects
+        ListBlocks();               //Detect which blocks are in a row and add them to the list
+        if (SyntaxCheck())      //Determine if syntax is correct
         {
             Debug.Log("Code can run");
         }
         else
         {
             Debug.Log("Code cannot run");
+            return;
+        }
+        CheckObjectToModify();
+        if (objectsToModify1.Count > 0)  //If there are any referenced objects
+        {
+            Debug.Log("There are objects to modify");
+            if (currentBlocks.Count > 1)                                            //If there are more than 1 blocks in the execution
+            {
+                if (currentBlocks[1].GetComponent<SP_CodeBlock_Info>().type == "Is") //If the starting block is followed by 'Is'
+                {
+                    Debug.Log("Object is followed by IS");
+                    if (currentBlocks[2].GetComponent<SP_CodeBlock_Info>().type == "Bool" ||
+                        currentBlocks[2].GetComponent<SP_CodeBlock_Info>().type == "Float" ||
+                        currentBlocks[2].GetComponent<SP_CodeBlock_Info>().type == "Object")    //If 'is' is followed by Bool, Float or Object
+                    {
+                        Debug.Log("IS is followed by valid block");
+                        ModifyObjects(currentBlocks[2].GetComponent<SP_CodeBlock_Info>()); //Call ModifyObjects and feed it the modification codeblock
+                    }
+                }
+            }
         }
     }
 
@@ -46,7 +70,7 @@ public class SP_CodeExecute : MonoBehaviour
         }
     }
 
-   bool SyntaxCheck()
+    bool SyntaxCheck()
     {
         if (currentBlocks[currentBlocks.Count-1].GetComponent<SP_CodeBlock_Info>().type == "Operator" ||
             currentBlocks[currentBlocks.Count - 1].GetComponent<SP_CodeBlock_Info>().type == "Is")
@@ -82,19 +106,68 @@ public class SP_CodeExecute : MonoBehaviour
                     }
                     break;
                 case "If":
-
+                    if (currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Int" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Bool" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Object")  //If the following block is correct
+                    {
+                        checkingBlock++;    //Move onto the next block
+                    }
+                    else
+                    {
+                        //HIGHLIGHT THIS BLOCK
+                        return false;
+                    }
                     break;
                 case "For":
-
+                    if (currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Int" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Bool" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Object")  //If the following block is correct
+                    {
+                        checkingBlock++;    //Move onto the next block
+                    }
+                    else
+                    {
+                        //HIGHLIGHT THIS BLOCK
+                        return false;
+                    }
                     break;
                 case "Int":
-
+                    if (currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Is" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Equals")  //If the following block is correct
+                    {
+                        checkingBlock++;    //Move onto the next block
+                    }
+                    else
+                    {
+                        //HIGHLIGHT THIS BLOCK
+                        return false;
+                    }
                     break;
                 case "Bool":
-
+                    if (currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Is" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Equals")  //If the following block is correct
+                    {
+                        checkingBlock++;    //Move onto the next block
+                    }
+                    else
+                    {
+                        //HIGHLIGHT THIS BLOCK
+                        return false;
+                    }
                     break;
                 case "Is":
-
+                    if (currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Int" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Bool" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Object" ||
+                        currentBlocks[checkingBlock + 1].GetComponent<SP_CodeBlock_Info>().type == "Float")  //If the following block is correct
+                    {
+                        checkingBlock++;    //Move onto the next block
+                    }
+                    else
+                    {
+                        //HIGHLIGHT THIS BLOCK
+                        return false;
+                    }
                     break;
                 default:
                     break;
@@ -102,5 +175,42 @@ public class SP_CodeExecute : MonoBehaviour
         }
         return true;
 
+    }
+
+    void CheckObjectToModify()
+    {
+        if (startingBlock.GetComponent<SP_CodeBlock_Info>().type == "Object")   //If the first code block is an object
+        {
+            foreach (GameObject referencedObject in GameObject.FindGameObjectsWithTag(startingBlock.GetComponent<SP_CodeBlock_Info>().referencedObjectsTag))    //Find all the objects referenced by the code block
+            {
+                objectsToModify1.Add(referencedObject);                                                                                                          //And add them to the objecftsToModify list
+            }
+        }
+    }
+
+    void ModifyObjects(SP_CodeBlock_Info modifier)
+    {
+        switch (modifier.type)
+        {
+            case "Bool":
+                foreach (GameObject parent in objectsToModify1)
+                {
+                    foreach (Transform child in parent.transform)
+                    {
+                        Debug.Log("Children are " + child.name);
+                        child.gameObject.SetActive(modifier._bool);
+                    }
+                }
+
+                break;
+            case "Float":
+                //MAKE THE OBJECTS FLOAT
+                break;
+            case "Object":
+                //CHANGE THE OBJECTS TO BE OTHER OBJECTS
+                break;
+            default:
+                break;
+        }
     }
 }
