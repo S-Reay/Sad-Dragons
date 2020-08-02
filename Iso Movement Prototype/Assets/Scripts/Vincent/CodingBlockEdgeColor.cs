66c3f9223be0a0de4c7c44c51da2b029a2a367c9
Vincent <3d314831994@gmail.com>
2020-08-02 12:09:46 +1000

    new visual effect.
    Exit tile effect, unmovable coding block highlight effect, moveable coding block twinkling effect.

---
+++ Iso Movement Prototype/Assets/Scripts/Vincent/CodingBlockEdgeColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodingBlockEdgeColor : MonoBehaviour
+{
+    private MeshRenderer blockRenender;
+    public Color32 originalColor = new Color32(84,84,84,255);
+    public Color32 newColor = new Color32(5,156,226,255);
+    public bool isConnected;
+    public float twinkleSpeed = 2f;
+    public Vector2 twinkleRange = new Vector2(0,0.75f);
+
+
+    void Start()
+    {
+        blockRenender = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (!isConnected)
+        {
+            float t = Mathf.Sin(twinkleSpeed * Time.time) * twinkleRange.magnitude;
+            blockRenender.material.color = Color.Lerp(originalColor, newColor, t);
+        }
+        else {
+            blockRenender.material.color = newColor;
+        }
+    }
+}
