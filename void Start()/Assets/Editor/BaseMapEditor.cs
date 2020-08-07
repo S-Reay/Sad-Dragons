bc14634182eb9201201915892d886da6d68d3a1d
Seth Reay <1017464@student.sae.edu.au>
2020-08-07 17:03:26 +1000

    RENAMED PROJECT
    Re-named the project folder, this causes no issues in Unity but will require every team member to re-add the project to their Unity Hub. (sorry guys lol)

---
+++ void Start()/Assets/Editor/BaseMapEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BaseMap))]
+public class BaseMapEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        BaseMap f = target as BaseMap;
+
+        if (GUILayout.Button("GenerateMap"))
+        {
+            f.GenerateFloor();
+        }
+
+    }
+
+}
