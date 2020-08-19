36fd0b140b4afe22037e2f194bf49ce93051d35c
Vincent <3d314831994@gmail.com>
2020-08-19 20:39:31 +1000

    Light exit tile, Door animations, Revised map editor.

--- void Start()/Assets/Scripts/Vincent/MapGenerate/BaseMap.cs
+++ void Start()/Assets/Scripts/Vincent/MapGenerate/BaseMap.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class BaseMap : MonoBehaviour
 {
+    public bool rotateRandomly;
+    public bool isMemorizingPosition;
     public GameObject floorTile;
     public Vector3 mapSize = new Vector3(10, 10, 1);
 
+    private int[] rotationValue = new int[] {0,90,180,270};
+
     public void GenerateFloor()
     {
-        ClearMap();
-        for (int x = 0; x < mapSize.x; x++)
-        {
-            for (int y = 0; y < mapSize.y; y++)
+        if (isMemorizingPosition) {
+            if (transform.childCount == (mapSize.x * mapSize.y * mapSize.z))
             {
-                for (int z = 0; z < mapSize.z; z++)
+                if (rotateRandomly)
                 {
-                    GameObject tile = Instantiate(floorTile,this.transform);
-                    tile.transform.localPosition = new Vector3(x, z, y);
+                    for (int i = 0; i < transform.childCount; i++)
+                    {
+                        Transform tile = transform.GetChild(i);
+                        int rotation = rotationValue[Random.Range(0, rotationValue.Length - 1)];
+                        tile.localEulerAngles = new Vector3(tile.localEulerAngles.x, rotation, 0);
+                    }
+                }
+            }
+        }
+        else
+        {
+            ClearMap();
+            for (int x = 0; x < mapSize.x; x++)
+            {
+                for (int y = 0; y < mapSize.y; y++)
+                {
+                    for (int z = 0; z < mapSize.z; z++)
+                    {
+                        if (rotateRandomly)
+                        {
+                            GameObject tile = Instantiate(floorTile, this.transform);
+                            tile.transform.localPosition = new Vector3(x, z, y);
+                            int rotation = rotationValue[Random.Range(0, rotationValue.Length - 1)];
+                            tile.transform.localEulerAngles = new Vector3(tile.transform.localEulerAngles.x, rotation, 0);
+                        }
+                        else
+                        {
+                            GameObject tile = Instantiate(floorTile, this.transform);
+                            tile.transform.localPosition = new Vector3(x, z, y);
+                        }
+
+                    }
                 }
             }
         }
