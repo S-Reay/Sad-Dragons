2844cd111a68e005a28140ceac47958912a445b5
Seth Reay <1017464@student.sae.edu.au>
2020-08-28 21:52:23 +1000

    Level 2.1 IF/THEN, Exportable Scenes

---
+++ void Start()/Assets/Scripts/Seth/Hardcoded If Statements/SP_2_1_IFTHEN.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SP_2_1_IFTHEN : MonoBehaviour
+{
+    [SerializeField]GameObject door;
+    [SerializeField] GameObject player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!door.activeSelf)
+        {
+            player.GetComponent<SP_Player_GridDirectionalMove>().isFlying = true;
+        }
+    }
+}
