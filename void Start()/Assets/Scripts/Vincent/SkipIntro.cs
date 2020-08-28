a50756e8b51f65c9523ee35994ca6a19c7c98435
Vincent <3d314831994@gmail.com>
2020-08-28 16:45:49 +1000

    New coding blocks color and lighting in intro cutscene.

---
+++ void Start()/Assets/Scripts/Vincent/SkipIntro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipIntro : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneController.i.LoadSceneBySec(2);
+        }
+    }
+}
